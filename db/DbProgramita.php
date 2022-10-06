7fd96e489a864cf7a69330a6bc008a92fb19d8d2
Omar Ibañez <omaribanez100@gmail.com>
2022-10-05 21:42:26 -0500

    cambios

--- db/DbProgramita.php
+++ db/DbProgramita.php
@@ -6,20 +6,55 @@ class DbProgramita extends DbConexion {
 	  
 	public function insertStores($id_usuario, $arr_datos, $tmp_filas){
 						
-			try{
-				$sql = "CALL pa_insert_stores('".$arr_datos."', ".$tmp_filas.", @id)";
-				echo($sql);
-				$arrCampos[0] = "@id";
-				$arrResultado = $this->ejecutarSentencia($sql, $arrCampos);
-				$resultado_out = $arrResultado["@id"];	
-				return $resultado_out;
+		try{
+
+			$sql = "CALL pa_insert_stores('".$arr_datos."', ".$tmp_filas.", @id)";
+			echo($sql);
+			$arrCampos[0] = "@id";
+			$arrResultado = $this->ejecutarSentencia($sql, $arrCampos);
+			$resultado_out = $arrResultado["@id"];	
+			return $resultado_out;
 					
-			} catch (Exception $e) {
-				return -2;
+		} catch (Exception $e) {
+			return -2;
 		}			
 	}
 
+	public function insertProducts($id_usuario, $arr_datos, $tmp_filas){
+						
+		try{
+			$sql = "CALL pa_insert_products('".$arr_datos."', ".$tmp_filas.", @id)";
+			echo($sql);
+			$arrCampos[0] = "@id";
+			$arrResultado = $this->ejecutarSentencia($sql, $arrCampos);
+			$resultado_out = $arrResultado["@id"];	
+			return $resultado_out;
+				
+		} catch (Exception $e) {
+			return -2;
+		}
+	}	
+
+	public function insertCategories($id_usuario, $arr_datos, $tmp_filas){
+						
+		try{
+			$sql = "CALL pa_insert_categories('".$arr_datos."', ".$tmp_filas.", @id)";
+			echo($sql);
+			$arrCampos[0] = "@id";
+			$arrResultado = $this->ejecutarSentencia($sql, $arrCampos);
+			$resultado_out = $arrResultado["@id"];	
+			return $resultado_out;
+				
+		} catch (Exception $e) {
+			return -2;
+		}
+	}
+
 	
+
+
+
 }
+	
 ?>
  
--- programita_ajax.php
+++ programita_ajax.php
@@ -95,7 +95,7 @@
 
             ?><input type="hidden" id="hdd_rta_import" name="hdd_rta_import" value="<?=  $rta ?>" /> <?php
         break;
-        case 3://Stores
+        case 3://Products
             @$archivo = str_decode($_POST["archivo"]);
             $ruta_archivo = "data/".$archivo;
             $contador = 0;
@@ -148,25 +148,170 @@
                                     if ($index == $filas) {
                                         $guarda = true;
                                     }
-                                    if ($store_id != "") {
+                                    if ($product_id != "") {
                                         if ($arr_datos != "") {
                                             $arr_datos .= "|";
                                         }
-                                        $arr_datos .= $increment.";".$product_id.";".";".$store_id.";".";".$code.";".$name.";".$img.";".$price.";".$discount.";".$tricoins.
+                                        $arr_datos .= $increment.";".$product_id.";".$store_id.";".$code.";".$name.";".$img.";".$price.";".$discount.";".$tricoins.
                                         ";".$premium_discount.";".$is_valid.";".$is_digital.";".$is_sustainable.";".$is_recyclable;
                                     }
                                 } else {
                                     if ($arr_datos != "") {
                                         $arr_datos .= "|";
                                     }
-                                        $arr_datos .= $increment.";".$store_id.";".$name.";".$nit.";".$phone.";".$email.";".$website.";".$address;
+                                        $arr_datos .= $increment.";".$product_id.";".$store_id.";".$code.";".$name.";".$img.";".$price.";".$discount.";".$tricoins.
+                                        ";".$premium_discount.";".$is_valid.";".$is_digital.";".$is_sustainable.";".$is_recyclable;
                                     $guarda = true;
                                 }
             
                                 if ($guarda) {
                                     $tmp_filas = count(explode("|", $arr_datos)); 
                                     
-                                    $rta = $dbProgramita->insertStores(5, $arr_datos, $tmp_filas);
+                                    $rta = $dbProgramita->insertProducts(5, $arr_datos, $tmp_filas);
+
+                                    if ($rta<0) { break; }
+            
+                                    $arr_datos = "";
+                                    $contador = 0;
+                                    $guarda = false;
+                                }
+
+                            }
+                        }else{
+                            $rta=-5;
+                        }
+                    }
+                }else{
+                    $rta=0;
+                }
+                   
+
+            }
+
+            ?><input type="hidden" id="hdd_rta_import" name="hdd_rta_import" value="<?=  $rta ?>" /> <?php
+        break;
+
+        case 4://Categories
+            @$archivo = str_decode($_POST["archivo"]);
+            $ruta_archivo = "data/".$archivo;
+            $contador = 0;
+            $arr_datos = "";
+            if ($fp = file($ruta_archivo)) {
+			
+                $filas = count($fp);
+                if ($filas > 0) {
+                    $columnas = 0;
+                    for ($index = 1; $index < 2; $index++) {
+                        $columnas = count(explode(";", $fp[$index]));
+                        if ($columnas == 2) {
+                            $id = "";
+                            $name = "";
+                           
+                            $arr_dato = array();
+                            for ($index = 1; $index <= $filas; $index++) {
+                                $contador++;
+                                $arr_dato = explode(";", $fp[$index]);
+                                $id = trim($arr_dato[0]);
+                                $name = trim($arr_dato[1]);
+                               
+                                $guarda = false;
+                                if ($contador < 1000) {
+                                    if ($index == $filas) {
+                                        $guarda = true;
+                                    }
+                                    if ($id != "") {
+                                        if ($arr_datos != "") {
+                                            $arr_datos .= "|";
+                                        }
+                                        $arr_datos .= $id.";".$name;
+                                    }
+                                } else {
+                                    if ($arr_datos != "") {
+                                        $arr_datos .= "|";
+                                    }
+                                        $arr_datos .= $id.";".$name;
+                                    $guarda = true;
+                                }
+            
+                                if ($guarda) {
+                                    $tmp_filas = count(explode("|", $arr_datos)); 
+                                    
+                                    $rta = $dbProgramita->insertCategories(5, $arr_datos, $tmp_filas);
+
+                                    if ($rta<0) { break; }
+            
+                                    $arr_datos = "";
+                                    $contador = 0;
+                                    $guarda = false;
+                                }
+
+                            }
+                        }else{
+                            $rta=-5;
+                        }
+                    }
+                }else{
+                    $rta=0;
+                }
+                   
+
+            }
+
+            ?><input type="hidden" id="hdd_rta_import" name="hdd_rta_import" value="<?=  $rta ?>" /> <?php
+        break;
+
+        case 5://Stocks
+            @$archivo = str_decode($_POST["archivo"]);
+            $ruta_archivo = "data/".$archivo;
+            $contador = 0;
+            $arr_datos = "";
+            if ($fp = file($ruta_archivo)) {
+			
+                $filas = count($fp);
+                if ($filas > 0) {
+                    $columnas = 0;
+                    for ($index = 1; $index < 2; $index++) {
+                        $columnas = count(explode(";", $fp[$index]));
+                        if ($columnas == 5) {
+                            $id = "";
+                            $product_id = "";
+                            $product_stock = "";
+                            $purchases = "";
+                            $store_id = "";
+                           
+                            $arr_dato = array();
+                            for ($index = 1; $index <= $filas; $index++) {
+                                $contador++;
+                                $arr_dato = explode(";", $fp[$index]);
+                                $id = trim($arr_dato[0]);
+                                $product_id = trim($arr_dato[1]);
+                                $product_stock = trim($arr_dato[2]);
+                                $purchases = trim($arr_dato[3]);
+                                $store_id = trim($arr_dato[4]);
+                               
+                                $guarda = false;
+                                if ($contador < 1000) {
+                                    if ($index == $filas) {
+                                        $guarda = true;
+                                    }
+                                    if ($id != "") {
+                                        if ($arr_datos != "") {
+                                            $arr_datos .= "|";
+                                        }
+                                        $arr_datos .= $id.";".$product_id.";".$product_stock.";".$purchases.";".$store_id;
+                                    }
+                                } else {
+                                    if ($arr_datos != "") {
+                                        $arr_datos .= "|";
+                                    }
+                                    $arr_datos .= $id.";".$product_id.";".$product_stock.";".$purchases.";".$store_id;
+                                    $guarda = true;
+                                }
+            
+                                if ($guarda) {
+                                    $tmp_filas = count(explode("|", $arr_datos)); 
+                                    
+                                    $rta = $dbProgramita->insertCategories(5, $arr_datos, $tmp_filas);
 
                                     if ($rta<0) { break; }
             
@@ -199,4 +344,6 @@
         $texto_rta = str_replace('"', "", $texto_rta);
         return $texto_rta;
     }   
+
+
 ?>
